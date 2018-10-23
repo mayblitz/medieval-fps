2fc48254867519bbbc833ecd8658568885411f5b
Alex Page <alex.page9005@gmail.com>
2018-10-23 22:27:33 +0300

    init

---
+++ DungeonCrawler/Assets/Scripts/EquippedAnimation.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EquippedAnimation : MonoBehaviour {
+
+    public CharacterController character;
+
+    Animator animator;
+    Vector3 lastPosition;
+    float tempTime;
+
+    // Use this for initialization
+    void Start () {
+        animator = GetComponent<Animator>();
+        lastPosition = character.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        tempTime += Time.deltaTime;
+
+        if (tempTime > 0.02)
+        {
+            if (!character.isGrounded)
+                animator.SetBool("isFalling", true);
+            else
+            {
+                animator.SetBool("isFalling", false);
+
+                if (lastPosition != character.transform.position)
+                {
+                    animator.SetBool("isMoving", true);
+                    lastPosition = character.transform.position;
+                }
+                else
+                {
+                    animator.SetBool("isMoving", false);
+                }
+            }
+            tempTime = 0;
+        }
+	}
+}
