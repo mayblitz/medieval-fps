9797dc7879b62a4d7f9a687fdb54bb337e789478
Alex Page <alex.page9005@gmail.com>
2018-10-24 15:01:30 +0300

    fix weapon clipping

---
+++ DungeonCrawler/Assets/Scripts/ClipCameraFollow.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClipCameraFollow : MonoBehaviour
+{
+    public Camera mainCamera;
+    public bool clampVerticalRotation = true;
+    public float ySensitivity = 2.5f;
+    private Camera clipCamera;
+    private Quaternion cameraTargetRot;
+
+    // Use this for initialization
+    void Start()
+    {
+        clipCamera = GetComponent<Camera>();
+        cameraTargetRot = clipCamera.transform.localRotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xRot = Input.GetAxis("Mouse Y") * ySensitivity;
+
+        cameraTargetRot *= Quaternion.Euler(-xRot, 0f, 0f);
+
+        cameraTargetRot = ClampRotationAroundXAxis(cameraTargetRot);
+
+        clipCamera.transform.localRotation = cameraTargetRot;
+    }
+
+    Quaternion ClampRotationAroundXAxis(Quaternion q)
+    {
+        q.x /= q.w;
+        q.y /= q.w;
+        q.z /= q.w;
+        q.w = 1.0f;
+
+        float angleX = 2.0f * Mathf.Rad2Deg * Mathf.Atan(q.x);
+
+        angleX = Mathf.Clamp(angleX, -112.5f, 112.5f);
+
+        q.x = Mathf.Tan(0.5f * Mathf.Deg2Rad * angleX);
+
+        return q;
+    }
+}
---
+++ DungeonCrawler/Assets/Scripts/PlayerAttack.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    public Transform attackPoint;
+
+    public void Attack()
+    {
+        var hits = Physics.OverlapSphere(attackPoint.position, 0.5f);
+
+        foreach (var hit in hits)
+        {
+            print(hit.name);
+        }
+    }
+}
