d341843eae74358924b93e41f7c637d02d5da281
Alexander Peychev <alex.page9005@gmail.com>
2018-11-01 10:55:16 +0200

    refactor player animations

--- DungeonCrawler/Assets/Scripts/AttackAnimation.cs
+++ DungeonCrawler/Assets/Scripts/AttackAnimation.cs
@@ -1,23 +1,20 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class AttackAnimation : MonoBehaviour
 {
     Animator animator;
 
-    // Use this for initialization
     void Start()
     {
         animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButton(0) && !animator.GetCurrentAnimatorStateInfo(0).IsName("Attack"))
+        if (Input.GetMouseButtonDown(0))
         {
-            animator.Play("Attack");
+            animator.SetLayerWeight(1, 1f);
+            animator.SetTrigger("isAttacking");
         }
     }
 }
