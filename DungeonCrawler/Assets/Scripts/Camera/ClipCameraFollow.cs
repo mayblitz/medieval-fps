775f77bac7b5097b00fc2f273a68969fbbc79f65
Alex Page <alex.page9005@gmail.com>
2018-11-19 23:42:27 +0200

    fix Player

--- DungeonCrawler/Assets/Scripts/Camera/ClipCameraFollow.cs
+++ DungeonCrawler/Assets/Scripts/Camera/ClipCameraFollow.cs
@@ -3,45 +3,19 @@
 public class ClipCameraFollow : MonoBehaviour
 {
     [SerializeField]
-    private string mouseYInputName = "Mouse Y";
-
-    [SerializeField]
-    private float mouseSensitivity = 150f;
-
-    private float xAxisClamp;
-
-    void Start()
-    {
-        mouseSensitivity = mouseSensitivity * 1.22f;
-        xAxisClamp = 0.0f;
-    }
+    private Camera mainCamera;
 
     void LateUpdate()
     {
-        float mouseY = Input.GetAxis(mouseYInputName) * mouseSensitivity * Time.deltaTime;
+        float mainX = mainCamera.transform.eulerAngles.x;
 
-        xAxisClamp += mouseY;
+        float x;
 
-        if (xAxisClamp > 110)
-        {
-            xAxisClamp = 110;
-            mouseY = 0.0f;
-            ClampXAxisRotationToValue(290);
-        }
-        else if (xAxisClamp < -110)
-        {
-            xAxisClamp = -110;
-            mouseY = 0.0f;
-            ClampXAxisRotationToValue(70);
-        }
+        if (mainX > 270.0f)
+            x = (mainX - 360.0f) * 1.2f;
+        else
+            x = mainX * 1.2f;
 
-        transform.Rotate(Vector3.left * mouseY);
-    }
-
-    private void ClampXAxisRotationToValue(float value)
-    {
-        Vector3 eulerRotation = transform.eulerAngles;
-        eulerRotation.x = value;
-        transform.eulerAngles = eulerRotation;
+        transform.eulerAngles = new Vector3(x, mainCamera.transform.eulerAngles.y, mainCamera.transform.eulerAngles.z);
     }
 }
---
+++ DungeonCrawler/Assets/Scripts/Player/PlayerLook.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+
+public class PlayerLook : MonoBehaviour
+{
+    [SerializeField]
+    private string mouseXInputName = "Mouse X", mouseYInputName = "Mouse Y";
+
+    [SerializeField]
+    private float mouseSensitivity = 100f;
+
+    [SerializeField]
+    private Transform playerBody;
+
+    private float xAxisClamp;
+
+    private void Awake()
+    {
+        LockCursor();
+        xAxisClamp = 0.0f;
+    }
+
+    private void Update()
+    {
+        CameraRotation();
+    }
+
+    private void LockCursor()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void CameraRotation()
+    {
+        float mouseX = Input.GetAxis(mouseXInputName) * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis(mouseYInputName) * mouseSensitivity * Time.deltaTime;
+
+        xAxisClamp += mouseY;
+
+        if (xAxisClamp > 80.0f)
+        {
+            xAxisClamp = 80.0f;
+            mouseY = 0.0f;
+            ClampXAxisRotationToValue(280.0f);
+        }
+        else if (xAxisClamp < -80.0f)
+        {
+            xAxisClamp = -80.0f;
+            mouseY = 0.0f;
+            ClampXAxisRotationToValue(80.0f);
+        }
+
+        transform.Rotate(Vector3.left * mouseY);
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+
+    private void ClampXAxisRotationToValue(float value)
+    {
+        Vector3 eulerRotation = transform.eulerAngles;
+        eulerRotation.x = value;
+        transform.eulerAngles = eulerRotation;
+    }
+}
